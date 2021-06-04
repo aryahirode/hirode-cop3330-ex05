f8cd8a45e1951b86c20ad7992558521ed4116c4f
Arya Hirode <aryahirode2001@gmail.com>
2021-06-04 19:04:57 -0400

    Working on output method

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -10,26 +10,24 @@ public class App {
     static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
         App myApp = new App();
-        myApp.output(myApp.num1Prompt(), myApp.num2Prompt());
+        myApp.calculations(myApp.num1Prompt(), myApp.num2Prompt());
     }
 
-    private String num1Prompt() {
+    private int num1Prompt() {
         System.out.print("What is the first number? ");
-        return in.nextLine();
+        return Integer.parseInt(in.nextLine());
     }
 
-    private String num2Prompt() {
+    private int num2Prompt() {
         System.out.print("What is the second number? ");
-        return in.nextLine();
+        return Integer.parseInt(in.nextLine());
     }
 
-    private int[] calculations() {
-        int num1 = Integer.parseInt(num1Prompt());
-        int num2 = Integer.parseInt(num2Prompt());
+    private int[] calculations(int num1, int num2) {
         return new int[]{num1 + num2, num1 - num2, num1 * num2, num1 / num2};
     }
 
-    private void output(String num1, String num2) {
-        System.out.printf("%s + %s = %d\n%s - %s = %d\n%s * %s = %d\n%s / %s = %d\n", num1, num2, calculations()[0], num1, num2, calculations()[1], num1, num2, calculations()[2], num1, num2, calculations()[3]);
+    private void output() {
+
     }
 }
